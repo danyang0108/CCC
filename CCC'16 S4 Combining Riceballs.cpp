4df80a67c8a15be35bd536c1ba8cab3eee168284
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2019-11-22 20:50:01 -0500

    Add files via upload

---
+++ CCC'16 S4 Combining Riceballs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,a[402],ans;bool dp[402][402];
+int main(){
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+        ans=max(ans,a[i]);
+        a[i]+=a[i-1]; dp[i][i]=true;
+    }
+    for (int len=1;len<n;len++){
+        for (int l=1;l+len<=n;l++){
+            int r=l+len;
+            for (int p=l,q=r;p<q;){
+                int ls=a[p]-a[l-1],rs=a[r]-a[q-1];
+                //join three
+                if (dp[l][p] && dp[q][r] && (p+1==q ||dp[p+1][q-1])&& ls==rs) dp[l][r]=true;
+                if (ls<rs)p++;
+                else q--;
+            }
+            if (dp[l][r])ans=max(ans,a[r]-a[l-1]);
+        }
+    }
+    printf("%d\n",ans);
+}
