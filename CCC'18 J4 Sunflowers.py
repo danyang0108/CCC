f682b36e82c422f1eae5e85b734ce4fa14cc3081
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2019-11-22 21:03:57 -0500

    Add files via upload

---
+++ CCC'18 J4 Sunflowers.py
@@ -0,0 +1,23 @@
+n=int(input())
+grid=[]
+for i in range(n):
+  grid.append(list(map(int,input().split())))
+edge=[grid[0][0],grid[0][n-1],grid[n-1][0],grid[n-1][n-1]]
+if max(edge)==edge[0]: #rotate 180
+  rotated = list(zip(*grid[::-1]))
+  rotated = list(zip(*rotated[::-1]))
+  for i in range(n):
+    print(' '.join(list(map(str,rotated[i]))))
+elif max(edge)==edge[1]:  #rotate 90
+  rotated = list(zip(*grid[::-1]))
+  for i in range(n):
+    print(' '.join(list(map(str,rotated[i]))))
+elif max(edge)==edge[2]: #rotate 270
+  rotated = list(zip(*grid[::-1]))
+  rotated = list(zip(*rotated[::-1]))
+  rotated = list(zip(*rotated[::-1]))
+  for i in range(n):
+    print(' '.join(list(map(str,rotated[i]))))
+else:
+  for i in range(n):
+    print(' '.join(list(map(str,grid[i]))))
