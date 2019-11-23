489b0535bc5da9689b465da1d29a84f8c7bd5bfe
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2019-11-22 21:07:42 -0500

    Add files via upload

---
+++ CCC'18 J1 Telemarketer or not.py
@@ -0,0 +1,20 @@
+digit=4
+if digit == 4:
+    x=int(input())
+    if x == 8 or x==9:
+        digit=digit-1
+    else:
+        print ("answer")
+if digit == 3:
+    y=int(input())
+    z=int(input())
+    if y==z:
+        digit=digit-2
+    else:
+        print ("answer")
+if digit == 1:
+    w=int(input())
+    if w==8 or w==9:
+        print ("ignore")
+    else: 
+        print ("answer")
