dccbcd80dcee74e5e4d5264f75d419f5404e8984
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2019-11-22 21:11:55 -0500

    Add files via upload

---
+++ CCC'19 S2 Pretty Average Primes.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prime[2000002];
+vector<int> primes;
+int t,n,num,se;
+int main(){
+    fill(prime,prime+2000001,true);
+    for (int i=2;i<=2000000;i++){
+        if (prime[i]) {
+            for (int j=i*2;j<=2000000;j+=i){
+                prime[j]=false;
+            }
+        }
+    }
+    for (int i=2;i<=1000000;i++){
+        if (prime[i])primes.push_back(i);
+    }
+    scanf("%d",&t);
+    for (int i=1;i<=t;i++){
+        scanf("%d",&n);
+        num=2*n;
+        for (auto u:primes){
+            se=num-u;
+            if (se>0 && se<2000000 && prime[se]){
+                printf("%d %d\n",u,se);
+                break;
+            }
+        }
+    }
+}
