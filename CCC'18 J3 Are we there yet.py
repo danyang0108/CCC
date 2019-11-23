ef508c132338317e854760878d150d1732aae776
danyang0108 <56311627+danyang0108@users.noreply.github.com>
2019-11-22 21:02:35 -0500

    Add files via upload

---
+++ CCC'18 J3 Are we there yet.py
@@ -0,0 +1,31 @@
+dis=list(map(int,input().split()))
+list1=dis.copy()
+count=0
+for i in range(5):
+  list1.insert(i,0)
+  if count==0:
+    for i in range(1,5):
+      list1[i]=list1[i]+list1[i-1]
+    print (list1[0],list1[1],list1[2],list1[3],list1[4])
+    list1=dis.copy()
+  elif count==1:
+    list1[3]=list1[2]+list1[3]
+    list1[4]=list1[3]+list1[4]
+    print (list1[0],list1[1],list1[2],list1[3],list1[4])
+    list1=dis.copy()
+  elif count==2:
+    list1[0]=list1[1]+list1[0]
+    list1[4]=list1[4]+list1[3]
+    print (list1[0],list1[1],list1[2],list1[3],list1[4])
+    list1=dis.copy()
+  elif count==3:
+    list1[0]=list1[0]+list1[1]+list1[2]
+    list1[1]=list1[1]+list1[2]
+    print (list1[0],list1[1],list1[2],list1[3],list1[4])
+    list1=dis.copy()
+  elif count==4:
+    list1[2]=list1[3]+list1[2]
+    list1[1]=list1[1]+list1[2]
+    list1[0]=list1[0]+list1[1]
+    print (list1[0],list1[1],list1[2],list1[3],list1[4])
+  count+=1
